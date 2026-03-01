8922898ae9f72372debfba1cc605a0d56f8ebbf6
SamuelBL101 <billysamuel101@gmail.com>
2026-03-01 11:11:27 +0100

    fix: remove nested backend folder

--- client-desktop/client-desktop/src/main/java/com/focusmonitor/client/clientdesktop/communication/UsageSender.java
+++ client-desktop/client-desktop/src/main/java/com/focusmonitor/client/clientdesktop/communication/UsageSender.java
@@ -7,13 +7,19 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
 
 public class UsageSender {
     private static final String API_URL = "http://localhost:8080/api/usage";
-
+    private static LocalDateTime timeSent = null;
     public static void sendUsage(UsageSession usageData) {
         try {
-
+            if(timeSent != null) {
+               long secondsSinceLastSend = java.time.Duration.between(timeSent, LocalDateTime.now()).getSeconds();
+               if(secondsSinceLastSend < 60) {
+                   return; // Ak uplynulo menej ako 60 sekúnd, neodosielaj
+               }
+            }
             HttpClient client = HttpClient.newHttpClient();
             String json = usageData.toJson();
             HttpRequest request = HttpRequest.newBuilder()
@@ -27,6 +33,8 @@ public class UsageSender {
                             System.out.println("Chyba pri odosielaní: " + response.statusCode());
                         } else {
                             System.out.println("Údaje úspešne odoslané.");
+                            timeSent = LocalDateTime.now();
+
                         }
                     });
 
